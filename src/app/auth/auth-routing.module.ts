15ec008a9e8a42739a26d2d26a2ceb54e5aabc7b
rukhsar624 <rukhsaranwar56@gmail.com>
2024-03-11 17:53:20 +0500

    add today code 3/11/2024

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuardGuard } from '../services/auth-guard.guard';
 import { AuthComponent } from './auth.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { LoginComponent } from './login/login.component';
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,42 +8,43 @@ import { HttpService } from 'src/app/services/http.service';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  public showFooter:boolean=true;
-  public showFooterAuth:boolean=true;
-constructor(private router:Router,private http: HttpService){
-  router.events.subscribe((val: any) => {
-    const routeName = router.routerState.snapshot.url.split("/")[2];
-    // console.log(routeName);
-    this.showFooter = !(routeName === 'home' || routeName === 'contact' || routeName === 'faqs');
-  });
-}
-ngOnInit(){
-  // this.observe()
-  // this.getCms()
-}
-// getCms() {
-//   try {
-//     this.http.get('settings/get', true).subscribe(
-//       (res: any) => {
-//         console.log(res);
-//         this.cms = res?.data;
-//       },
-//       (error) => {
-//         console.error('Error fetching user:', error);
-//       }
-//     );
-//   } catch (error) {
-//     console.error('Error fetching user:', error);
-//   }
-// }
+  public showFooter: boolean = true;
+  public showFooterAuth: boolean = true;
+  public cms: any;
+  constructor(private router: Router, private http: HttpService) {
+    router.events.subscribe((val: any) => {
+      const routeName = router.routerState.snapshot.url.split("/")[2];
+      // console.log(routeName);
+      this.showFooter = !(routeName === 'home' || routeName === 'contact' || routeName === 'faqs');
+    });
+  }
+  ngOnInit() {
+    // this.observe()
+    this.getCms()
+  }
+  getCms() {
+    try {
+      this.http.get('/get-copyright', true).subscribe(
+        (res: any) => {
+          console.log(res);
+          this.cms = res?.copyright;
+        },
+        (error) => {
+          console.error('Error fetching user:', error);
+        }
+      );
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
+  }
 
 
-// async observe() {
-//   UniversalService.settingsUpdate.subscribe((res: any)=>{
-//     if(res){
-//       this.getCms();
-//     }
-//     this.cd.detectChanges();
-//   })
-// }
+  // async observe() {
+  //   UniversalService.settingsUpdate.subscribe((res: any)=>{
+  //     if(res){
+  //       this.getCms();
+  //     }
+  //     this.cd.detectChanges();
+  //   })
+  // }
 }
